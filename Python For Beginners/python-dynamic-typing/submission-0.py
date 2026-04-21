b19d8b105d13bd2e871af35113287d40421780a7
Ryan Gibson <ryan@gibs.gg>
2026-04-21 11:00:56 -0500

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 10
+print(type(variable))
+
+variable = 10.5
+print(type(variable))
+
+variable = True
+print(type(variable))
+
+variable = "Hi"
+print(type(variable))
+
+variable = [1, 2]
+print(type(variable))
