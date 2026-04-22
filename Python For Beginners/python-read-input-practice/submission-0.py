e6fd71c379dde053ce5cdc5f1468625bb2b9694e
Ryan Gibson <ryan@gibs.gg>
2026-04-22 00:00:12 -0500

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,13 @@
+def add_two_numbers() -> int:
+    inputs = input().split(',')
+    x, y = int(inputs[0]), int(inputs[1])
+
+    return x + y
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
